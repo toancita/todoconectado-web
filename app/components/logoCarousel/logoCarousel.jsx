c745e5efdedc9f5310d658e145aff0f6e5370b4e
toancita <antomartinamanzanares@gmail.com>
2025-07-19 16:23:16 -0400

    Style: format logo carousel component and enhance dark mode support

--- app/components/logoCarousel/logoCarousel.jsx
+++ app/components/logoCarousel/logoCarousel.jsx
@@ -6,33 +6,29 @@ import styles from './logoCarousel.module.css'
 
 const LogoCarousel = () => {
     
-    return (
-        <Slider
-            width="250px"
-            duration={20}
-            pauseOnHover={true}
-            blurBorders={false}
-            blurBorderColor={'#fff'}>
-                
-            <Slider.Slide><img src="/slider/logo1.svg" alt="any" className={styles.logo} /></Slider.Slide>
-            <Slider.Slide><img src="/slider/logo2.svg" alt="any2" className={styles.logo} /></Slider.Slide>
-            <Slider.Slide><img src="/slider/logo3.svg" alt="any3" className={styles.logo} /></Slider.Slide>
-            <Slider.Slide><img src="/slider/logo4.svg" alt="any3" className={styles.logo} /></Slider.Slide>
-
-
-        </Slider>
-    )
-}              
+  return (
+    <Slider
+        width="250px"
+        duration={20}
+        pauseOnHover={true}
+        blurBorders={false}
+        blurBorderColor={'#fff'}>
+            
+        <Slider.Slide><img src="/slider/logo1.svg" alt="any" className={styles.logo} /></Slider.Slide>
+        <Slider.Slide><img src="/slider/logo2.svg" alt="any2" className={styles.logo} /></Slider.Slide>
+        <Slider.Slide><img src="/slider/logo3.svg" alt="any3" className={styles.logo} /></Slider.Slide>
+        <Slider.Slide><img src="/slider/logo4.svg" alt="any3" className={styles.logo} /></Slider.Slide>
+    </Slider>
+  )}              
                      
 export default function Brands() {
 
-    return(
-        <section className={styles.brandsContainer}>
-            <h1 className={styles.title}>Confían en nosotros</h1>
-            
-            <div>
-                <LogoCarousel />
-            </div>
-        </section>
-    )
+  return(
+    <section className={styles.brandsContainer}>
+        <h1 className={styles.title}>Confían en nosotros</h1>
+        <div>
+            <LogoCarousel />
+        </div>
+    </section>
+  )
 }
