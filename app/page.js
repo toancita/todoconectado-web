1efae29be19c09b4884e39353e69cb280eafba8c
Jose Guerra <jose.guerra@bituin.cl>
2025-07-19 01:47:38 -0400

    feat: add about us section

--- app/page.js
+++ app/page.js
@@ -1,18 +1,16 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import NavBar from "/app/components/navigationBar/navigationBar.jsx";
 import Hero from "/app/components/hero/hero.jsx";
 import Services from "./components/services/services";
+import AboutUs from "./components/aboutUs/aboutUs";
 
 export default function Home() {
   return (
-    
     <div className={styles.page}>
-      
       <NavBar/>
       <Hero/>
       <Services/>
-      
+      <AboutUs />
     </div>
   );
 }
