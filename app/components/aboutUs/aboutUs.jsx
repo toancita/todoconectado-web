a88565d42fcd876c1e713a12875d27a49ff1921a
Jose Guerra <jose.guerra02@inacapmail.cl>
2025-07-23 22:03:09 -0400

    hotfix: about us section image and styles

---
+++ app/components/aboutUs/aboutUs.jsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Image from 'next/image';
+import styles from './aboutUs.module.css'
+
+export default function AboutUs() { 
+  return (
+    <section className={styles.aboutUsContainer}>
+      <div className={styles.leftContent}>
+      <span className={styles.subtitle}>QUIÉNES SOMOS</span>
+        <div className={styles.contentContainer}>
+          <h2 className={styles.title}>Comprometidos con la tecnología, <span className={styles.highlight}>conectados</span> contigo</h2>
+          <p className={styles.content}> En Todo Conectado transformamos ideas en soluciones. Somos una empresa apasionada por la tecnología,
+            enfocada en brindar servicios personalizados que simplifican procesos y mejoran la calidad de vida.
+            Trabajamos para convertirnos en un aliado confiable en el camino hacia la transformación digital.
+          </p>
+          <a className={styles.button} href='#servicios'>
+            Conoce más
+          </a>
+        </div>
+      </div>
+      <div className={styles.rightContent}>
+        <div className={styles.imageBackground}></div>
+        <Image
+          src="/about_us_image.png"
+          alt="About us"
+          width={600}
+          height={600}
+          className={styles.image}
+        />
+      </div>
+    </section>
+  );
+
+}
