6f603f536960e9eb630fc8e79a0c9ace6ec716f4
Jose Guerra <jguerra@bituin.cl>
2025-10-07 23:30:19 -0300

    fix: mobile about us and hero sections

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -31,7 +31,7 @@ export default function Footer() {
         </div>
 
         <div className={styles.section}>
-          <h3 className="title">Contacto</h3>
+          <h3 className={styles.title}>Contacto</h3>
           <address className={styles.address}>
               <div className={styles.contactItem}>
                   <Mail/> 
@@ -43,7 +43,7 @@ export default function Footer() {
               </div>
               <div className={styles.contactItem}>
                   <MapPin/>
-                  <p>Arica, Chile. Lomas del miramar 040</p>
+                  <p>Arica, Chile. Panamericana Norte 3699</p>
               </div>
           </address>
         </div>
@@ -52,10 +52,10 @@ export default function Footer() {
       <div className={styles.copyrightContainer}>
           <p>© {new Date().getFullYear()} Todo Conectado.</p>
           
-          <div className={styles.legalLinks}>
-          <Link href="/privacidad" className={styles.legalLink}>Política de Privacidad</Link>
-          <Link href="/terminos" className={styles.legalLink}>Términos y Condiciones</Link>
-          </div>
+          {/* <div className={styles.legalLinks}>
+            <Link href="/privacidad" className={styles.legalLink}>Política de Privacidad</Link>
+            <Link href="/terminos" className={styles.legalLink}>Términos y Condiciones</Link>
+          </div> */}
       </div>
     </footer>
   )
