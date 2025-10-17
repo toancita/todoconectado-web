81677df99121767b6f851010562ad21b41f6f914
toancita <antomartinamanzanares@gmail.com>
2025-10-16 21:45:43 -0300

    Update: add new WEBP and SVG logos for enhanced visual representation in the slider.

--- app/components/logoCarousel/logoCarousel.jsx
+++ app/components/logoCarousel/logoCarousel.jsx
@@ -26,7 +26,7 @@ const LogoCarousel = () => {
 export default function Brands({ showTitle = true }) {
   return(
     <section id="clients" className={styles.brandsContainer}>
-       {showTitle && <h1 className={styles.title}>Confían en nosotros</h1>}
+       {showTitle && <h1 className={styles.title}>Colaboradores</h1>}
       <div>
         <LogoCarousel />
       </div>
